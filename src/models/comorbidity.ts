3c8654534197e4bd27140e0fa50cc33c952cdb2b
Dan DeMaria <ddemaria@oicr.on.ca>
2024-05-15 17:52:19 -0400

    Unused Imports

--- src/models/comorbidity.ts
+++ src/models/comorbidity.ts
@@ -1,25 +1,24 @@
 import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  Generated,
-  PrimaryGeneratedColumn,
+	Column,
+	CreateDateColumn,
+	Entity,
+	PrimaryGeneratedColumn,
 } from "typeorm";
 
 @Entity({ name: "comorbidity" })
 export class Comorbidity {
-  @PrimaryGeneratedColumn()
-  id: string;
-  @CreateDateColumn()
-  created_at: Date;
-  @Column()
-  programId?: string;
-  @Column()
-  submitterDonorId?: string;
-  @Column()
-  entityType?: string;
-  @Column()
-  entityId: string;
-  @Column()
-  comorbidityTypeCode?: string;
+	@PrimaryGeneratedColumn()
+	id: string;
+	@CreateDateColumn()
+	created_at: Date;
+	@Column()
+	programId?: string;
+	@Column()
+	submitterDonorId?: string;
+	@Column()
+	entityType?: string;
+	@Column()
+	entityId: string;
+	@Column()
+	comorbidityTypeCode?: string;
 }
