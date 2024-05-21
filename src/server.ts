88c1881393e0bff5b626b3c75d97037003e90cd3
UmmulkiramR <urangwala@oicr.on.ca>
2024-05-21 18:55:43 -0400

    added healthcheck endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,23 @@
 import {beginMigration} from "./migration.js";
 import express from 'express';
+import {Request, Response } from 'express';
 import {triggerDataCorrection} from "./data-correction.js";
 const app = express();
 
 app.route('/migrate').get(beginMigration);
 app.route('/correct').get(triggerDataCorrection);
+app.route('/alive').get(beginMigration);
+
 app.listen(process.env['PORT'], () => {
     console.log(`HTTP REST API SERVER available at http://localhost:${process.env['PORT']}`);
 });
 
 console.log('Server started successfully');
+
+function healthCheck(request: Request, response: Response) {
+        const healthcheck = {
+            message: 'Service Up',
+            timestamp: Date.now(),
+        };
+        return response.send(healthcheck);
+    }
